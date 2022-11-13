70722cddc9f37fa8c696e5dd75d7f4d41622daac
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-13 05:21:36 +0100

    Modification: Created App components and App routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Home from "./routes/homepage/Home";
+import Placetobe from "./routes/placetobe/Placetobe";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="main-container">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/Placetobe" element={<Placetobe />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
