fdda32f15344eca0bde59781c2f36b0daee2543a
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-15 10:14:38 +0100

    modification: modification the showcase card session and made it fully responsive created new file: src/Data.js containing info for the cards

--- src/components/showcase/Showcase.js
+++ src/components/showcase/Showcase.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Showcase.css";
+import MBToken from "../../images/home images/mbtoken.svg";
+import MetaMask from "../../images/home images/metamask.svg";
+import Opensea from "../../images/home images/opensea.svg";
+import Card from "../card/Card";
+import Data from "../../Data";
 
 const Showcase = () => {
-  return <div>Showcase</div>;
+  return (
+    <section className="showcase-container">
+      <div className="logo-section">
+        <img src={MBToken} alt="MBToken logo" className="mbtoken" />
+        <img src={MetaMask} alt="METAMASK logo" className="metamask" />
+        <img src={Opensea} alt="OpenSea logo" className="opensea" />
+      </div>
+
+      <h2 className="showcase-header">Inspiration for your next adventure</h2>
+
+      <div className="grid-section">
+        {Data.map((card, index) => {
+          return <Card card={card} id={index} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Showcase;
