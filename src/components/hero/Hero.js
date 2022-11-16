946efe816f2938a7cdbe517adcbbe6ffa2d50084
Tega Chris Akpovbiyor <96153123+heytega@users.noreply.github.com>
2022-11-16 17:29:49 +0100

    Update Hero.js
    Modification: modified the animation on the hero images

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -44,8 +44,8 @@ const Hero = () => {
         <div className="image-grid">
           <img data-aos="fade-down" src={Image1} alt="" className="image-1" />
           <img data-aos="fade-left" src={Image2} alt="" className="image-2" />
-          <img data-aos="fade-right" src={Image3} alt="" className="image-3" />
-          <img data-aos="fade-up" src={Image4} alt="" className="image-4" />
+          <img data-aos="fade-up" src={Image3} alt="" className="image-3" />
+          <img data-aos="fade-right" src={Image4} alt="" className="image-4" />
         </div>
       </div>
     </section>
